ac3a148253c9431d923e5a5a656a20a2022a37d1
Matias Bergerman <matias.bergerman@hotmail.com>
2021-04-16 15:01:13 -0300

    Create PyCharm project

---
+++ src/app.py
@@ -0,0 +1,11 @@
+# PyQt5 modules
+from PyQt5 import QtWidgets
+# Python modules
+import sys
+# Main window ui import
+from src.mainwindow import MainWindow
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
