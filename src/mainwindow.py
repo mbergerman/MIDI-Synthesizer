aa303464ecad2b741f914681e80a6027010b2a46
Matias Bergerman <matias.bergerman@hotmail.com>
2021-05-09 23:07:55 -0300

    Empecé a testear el tema de la lista de tracks en la GUI

--- src/mainwindow.py
+++ src/mainwindow.py
@@ -2,7 +2,29 @@
 from PyQt5.QtWidgets import QMainWindow
 # Project modules
 from src.ui.mainwindow import Ui_MainWindow
+
+from src.TrackItemWidget import *
+
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.setupUi(self)
+
+        item = QtWidgets.QListWidgetItem(self.track_list)
+        self.track_list.addItem(item)
+        row = TrackItemWidget('Track 01')
+        item.setSizeHint(row.minimumSizeHint())
+        self.track_list.setItemWidget(item, row)
+
+        item = QtWidgets.QListWidgetItem(self.track_list)
+        self.track_list.addItem(item)
+        row = TrackItemWidget('Track 02')
+        item.setSizeHint(row.minimumSizeHint())
+        self.track_list.setItemWidget(item, row)
+
+
+        item = QtWidgets.QListWidgetItem(self.track_list)
+        self.track_list.addItem(item)
+        row = TrackItemWidget('Track 03')
+        item.setSizeHint(row.minimumSizeHint())
+        self.track_list.setItemWidget(item, row)
